72fa3d7a2915d63eac7c4e5b39e88bed95b9c0a3
supernovah <supawat.udoms@gmail.com>
2025-12-18 20:52:36 +0700

    lab8_1 test

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -12,6 +12,7 @@ int main()
     case 'B': cout << "You have received 1 gems.\n";
     case 'C': cout << "You have received 2000 coins.\n";
     case 'D': cout << "You have received very KAK items.\n";
+    
   }
   return 0;
 }
